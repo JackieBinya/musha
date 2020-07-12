d45db55c8ef1a3c17c87477bf6e023a52bbe7e95
JackieBinya <jacquelinebinya@gmail.com>
2020-07-12 21:56:25 +0200

    Adds route users are redirected to after logout

--- src/pages/Properties.js
+++ src/pages/Properties.js
@@ -3,18 +3,22 @@ import { firebase } from '../firebase';
 
 /* 
 Authenticated users access a form they can use to list a new property
-Users get their listed properties by id
+Users get their listed properties by id'
 Users can update their properties
 */
 
-export const Properties = () => {
+export const Properties = ({ history }) => {
   const handleLogOut = () => {
     firebase.auth().signOut();
+    history.push("/");
   };
 
   return (
+    <>
+    <h1>Properties</h1>
     <button type="button" onClick={handleLogOut}>
       Logout
     </button>
+    </>
   );
 };
