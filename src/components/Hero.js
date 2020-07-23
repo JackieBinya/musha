a85d01ac054531c0ef51c1bc14f2eb6baa9e62c5
JackieBinya <jacquelinebinya@gmail.com>
2020-07-23 11:11:16 +0200

    Adds hero

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero">
+        <div className="hero-overlay">
+          <div className="hero-content">
+            <p>mushaa your property champions since 2017</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,40 +3,45 @@ import { Link } from 'react-router-dom';
 import { useProperties } from '../hooks';
 import { PropertyIcons } from '../components/PropertyIcons';
 import { ShortProperty } from '../components/ShortProperty';
+import { Hero } from '../components/Hero';
 
 export const Home = () => {
   const { properties } = useProperties();
   return (
     <>
-      <h2>Home</h2>
-      {properties.map(
-        ({
-          id,
-          location,
-          imageUrls,
-          numberOfBathroom,
-          numberOfBathrooms,
-          numberOfBedrooms,
-          city,
-          title,
-        }) => (
-          <li key={id}>
-            <Link to={`/property/${id}`}>
-              {' '}
-              {title ? `${title}` : `New property in ${location}`}
-            </Link>
+      <Hero />
+      <main>
+        <div className="container">
+          {properties.map(
+            ({
+              id,
+              location,
+              imageUrls,
+              numberOfBathroom,
+              numberOfBathrooms,
+              numberOfBedrooms,
+              city,
+              title,
+            }) => (
+              <li key={id}>
+                <Link to={`/property/${id}`}>
+                  {' '}
+                  {title ? `${title}` : `New property in ${location}`}
+                </Link>
 
-            <ShortProperty imageUrls={imageUrls}>
-              <PropertyIcons
-                city={city}
-                location={location}
-                numberOfBathrooms={numberOfBathrooms}
-                numberOfBedrooms={numberOfBedrooms}
-              />
-            </ShortProperty>
-          </li>
-        )
-      )}
+                <ShortProperty imageUrls={imageUrls}>
+                  <PropertyIcons
+                    city={city}
+                    location={location}
+                    numberOfBathrooms={numberOfBathrooms}
+                    numberOfBedrooms={numberOfBedrooms}
+                  />
+                </ShortProperty>
+              </li>
+            )
+          )}
+        </div>
+      </main>
     </>
   );
 };
