c6b299d9745aa09621bff42834e1c723f1ab2ce0
JackieBinya <jacquelinebinya@gmail.com>
2020-08-14 08:51:31 +0200

    Persist authentication state in local storage

--- src/context/auth-context/index.js
+++ src/context/auth-context/index.js
@@ -5,11 +5,23 @@ import { firebase } from '../../firebase';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem('currentUser'))
+  );
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged(setCurrentUser);
+    localStorage.setItem('currentUser', JSON.stringify(currentUser));
   }, []);
+
+  useEffect(() => {
+    if(currentUser === null){
+      localStorage.removeItem('currentUser');
+    }
+    if(currentUser){
+      localStorage.setItem('currentUser', JSON.stringify(currentUser));
+    }
+  }, [currentUser]);
 
   return (
     <AuthContext.Provider value={{ currentUser }}>
