f55a7d046072f8dcdef6bdfde1253b03021f375c
JackieBinya <jacquelinebinya@gmail.com>
2020-08-31 14:15:12 +0200

    Set up for firebase deployment

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DB_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyDD14Pef51sWG0czExRugPOLJO-Ne44bNI",
+  authDomain: "musha-000.firebaseapp.com",
+  databaseURL: "https://musha-000.firebaseio.com",
+  projectId: "musha-000",
+  storageBucket:  "musha-000.appspot.com",
+  messagingSenderId: "111803589651",
+  appId: "1:111803589651:web:b90b953a233696281b4be6",
 });
 
 const storage = firebase.storage();
