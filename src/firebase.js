865049fe9acc5fc09dbfa1bb64c0256a288b3392
JackieBinya <jacquelinebinya@gmail.com>
2020-08-31 15:00:29 +0200

    Ran a linter

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "AIzaSyDD14Pef51sWG0czExRugPOLJO-Ne44bNI",
-  authDomain: "musha-000.firebaseapp.com",
-  databaseURL: "https://musha-000.firebaseio.com",
-  projectId: "musha-000",
-  storageBucket:  "musha-000.appspot.com",
-  messagingSenderId: "111803589651",
-  appId: "1:111803589651:web:b90b953a233696281b4be6",
+  apiKey: 'AIzaSyDD14Pef51sWG0czExRugPOLJO-Ne44bNI',
+  authDomain: 'musha-000.firebaseapp.com',
+  databaseURL: 'https://musha-000.firebaseio.com',
+  projectId: 'musha-000',
+  storageBucket: 'musha-000.appspot.com',
+  messagingSenderId: '111803589651',
+  appId: '1:111803589651:web:b90b953a233696281b4be6',
 });
 
 const storage = firebase.storage();
